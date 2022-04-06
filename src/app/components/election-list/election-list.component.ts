309fb4a329e379793e49c3704a8826310ae7f9a3
arthur.azevedo <arthur.azevedo@accenture.com>
2022-04-06 10:20:44 -0300

    Criando Listagem de Eleições #2

--- src/app/components/election-list/election-list.component.ts
+++ src/app/components/election-list/election-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Election } from 'src/app/model';
+import { ElectionService } from 'src/app/services';
 
 @Component({
   selector: 'app-election-list',
   templateUrl: './election-list.component.html',
-  styleUrls: ['./election-list.component.scss']
+  styleUrls: ['./election-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ElectionListComponent implements OnInit {
 
-  constructor() { }
+  electionList: BehaviorSubject<Election[] | null> = new BehaviorSubject<Election[] | null>(null);
+
+  constructor(
+    private electionService: ElectionService
+  ) { }
 
   ngOnInit(): void {
+    this.electionService.getElections().subscribe(
+      electionList => this.electionList.next(electionList)
+    );
   }
 
 }
--- src/app/services/election.service.ts
+++ src/app/services/election.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Election } from '../model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ElectionService {
 
-  constructor() { }
+  private readonly PATH = environment.API_ADDRESS + '/election';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getElections(): Observable<Election[]> {
+    return this.http.get<Election[]>(this.PATH);
+  }
+
+  save(election: Election): Observable<Election> {
+    return this.http.post<Election>(this.PATH, election);
+  }
 }
